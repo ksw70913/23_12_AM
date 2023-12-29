981da52d8e08857d5598a988bf7ef81f267437c6
ksw70913 <pavico0913@gmail.com>
2023-12-29 13:13:34 +0900

    1st, 게시글 작성 추가

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,45 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("== 프로그램 시작 ==");
+		System.out.println("== 프로그램 시작 == ");
 
 		Scanner sc = new Scanner(System.in);
 
-		String cmd = sc.next();
-		cmd = sc.next();
+		int lastArticleId = 0;
 
-		System.out.println("== 프로그램 끝 ==");
+		while (true) {
+			System.out.print("명령어 > ");
+			String cmd = sc.nextLine().trim();
+
+			if (cmd.length() == 0) {
+				System.out.println("명령어를 입력하세요");
+				continue;
+			}
+
+			if (cmd.equals("exit")) {
+				break;
+			}
+			if (cmd.equals("article write")) {
+				System.out.println("==게시글 작성==");
+				int id = lastArticleId + 1;
+				System.out.print("제목 : ");
+				String title = sc.nextLine();
+				System.out.print("내용 : ");
+				String body = sc.nextLine();
+
+//				System.out.println(title + " / " + body);
+
+				System.out.printf("%d번 글이 생성 되었습니다.\n", id);
+				lastArticleId++;
+			} else if (cmd.equals("article list")) {
+				System.out.println("==게시글 목록==");
+			} else {
+				System.out.println("사용할 수 없는 명령어입니다");
+			}
+		}
+
+		System.out.println("== 프로그램 끝 == ");
+
+		sc.close();
 	}
 }
